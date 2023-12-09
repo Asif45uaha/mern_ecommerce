4c8e8d7487b88fbe48b99139d005fa52add3cbe3
Asif Ali Mir <asif15310@gmail.com>
2023-12-09 20:07:35 +0530

    third

--- client/src/pages/PaymentSuccess.jsx
+++ client/src/pages/PaymentSuccess.jsx
@@ -1,14 +1,14 @@
 import { Button } from "@material-tailwind/react"
-import { useSearchParams, useNavigate } from "react-router-dom"
+import { useNavigate } from "react-router-dom"
 const PaymentSuccess = () => {
-    const query = useSearchParams()[0]
+
     const navigate = useNavigate()
-    const referenceNum = query.get("reference")
+
     return (
         <div className="flex flex-col  font-Poppins justify-center items-center h-screen px-6 bg-gray-300">
             <div className=" shadow-md bg-white p-4  justify-center">
-                <p className="font-bold">Dear Customer We have Received  Your Payment </p>
-                <p>Order reference Number is : {referenceNum}</p>
+                <p className="font-bold">Dear Customer We have Received  Your Payment</p>
+                <p>Your Order has been Confirmed and is Ready to Be shipped</p>
                 <Button onClick={() => navigate("/orders")} className="mx-auto py-2">Confirm Order</Button>
             </div>
 
